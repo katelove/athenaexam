c8d3984ec359c88e79da4cbf7a7b4bfd22e9c75f
Kate <katelovejesus1110@gmail.com>
2021-07-16 19:52:13 +0800

    設定 bootstrap

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
 Vue.config.productionTip = false
-
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
