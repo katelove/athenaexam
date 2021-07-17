67bcd5eb4f5093fe98951ba9307457c0d5200531
Kate <katelovejesus1110@gmail.com>
2021-07-17 10:43:39 +0800

    新增table 樣式

--- src/components/TableGrades.vue
+++ src/components/TableGrades.vue
@@ -1,21 +1,61 @@
 <template>
-<div>
   <table>
     <thead>
       <tr>
-        <th v-for='(item,index) in titleList' :key="index">{{item}}</th>
+        <th v-for="(item, index) in titleList" :key="index">
+          {{ item }}
+        </th>
+      </tr>
+      <tr>
+        <th>
+          <button class="bluePlus"><h4>+</h4></button>
+        </th>
+        <th v-for="(item, index) in inputList" :key="index">
+          <input type="text" />
+        </th>
+        <th></th>
+        <th></th>
       </tr>
     </thead>
+    <tbody>
+      <tr>
+        <th></th>
+      </tr>
+    </tbody>
   </table>
-</div>
 </template>
 
 <script>
 export default {
+  name: 'TableGrades',
+  props: ['name', 'email', 'english', 'math'],
   data () {
     return {
-      titleList: ['動作', '姓名', 'email', '英文', '數學', '時間']
+      titleList: [
+        '動作',
+        '姓名',
+        'email',
+        '英文',
+        '數學',
+        '新增時間',
+        '修改時間'
+      ],
+      inputList: [] // 資料
+    }
+  },
+  mounted () {
+    // input欄位有4個
+    for (var i = 0; i < 4; i++) {
+      this.inputList.push({
+        name: '',
+        email: '',
+        english: '',
+        math: ''
+      })
     }
   }
 }
 </script>
+<style lang="scss">
+@import '../scss/tableGrades.scss';
+</style>
